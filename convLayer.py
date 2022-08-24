96d556ddac7a25a690d9f2c9b7dd839240b946b5
Aamir <zaikali22@gmail.com>
2022-08-24 09:20:55 +0530

    Files Added

--- convLayer.py
+++ convLayer.py
@@ -1,14 +1,21 @@
 import tensorflow as tf 
+import numpy as np
 
 class Convolve2D(tf.keras.layers.Layer):
-    def __init__(self,padding,kernels, filters, strides=(1,1)) -> None:
-        super(Convolve2D).__init__()
-        self.padding = padding 
-        self.convLayer = tf.keras.layers.Conv2D(filters=filters, kernel_size=kernels, strides=strides)
+    def __init__(self,kernels, filters, padding,  strides=(1,1)) -> None:
+        super(Convolve2D,self).__init__()
+        self.padding = padding
+        self.pads = [self.padding,self.padding] 
+        self.paddings =[[0, 0], self.pads, self.pads , [0,0]]
+        self.kernels = kernels
+        self.filters = filters
+        self.strides = strides
 
-    def call(self,input):
-        pads = [self.padding,self.padding]
-        paddings =[pads, pads , [0,0]]
-        padded = tf.pad(input, paddings , 'CONSTANT')
-        return self.convLayer(padded)
-        
+    def build(self,inputs):
+        channels = inputs[3]
+        self.kernel =  tf.Variable(lambda: tf.random.normal([self.kernels,self.kernels, channels,self.filters]), trainable=True)
+
+    def call(self,inputs):
+        print(inputs.shape[1:])
+        x = tf.nn.conv2d(inputs, filters=self.kernel, strides=[1,self.strides, self.strides, 1], padding = self.paddings)   
+        return x  
--- model.py
+++ model.py
@@ -1,3 +1,49 @@
 import tensorflow as tf 
 from convLayer import Convolve2D
+import json
 
+tf.keras.backend.clear_session()
+with open('./architecture.json','r') as f:
+    arc = json.load(f)
+
+inp = tf.keras.Input((448,448,3))
+
+firstLayer = True
+
+for key in arc:
+    repetitions = int(key.split('-')[-1])
+    i=0
+    while i<repetitions:
+        structure = arc[key]
+        for struct in structure:
+            if struct['pooling']!=1:
+                kernels, filters, paddings, strides = struct['kernel'], struct['filters'], struct['padding'], struct['strides']
+                # print(strides)
+                if firstLayer:
+                    x =  Convolve2D(kernels, filters, paddings, strides)(inp)
+                    x = tf.keras.layers.BatchNormalization()(x)
+                    x = tf.keras.layers.LeakyReLU(0.1)(x) 
+                    firstLayer = False
+                else:
+                    x =  Convolve2D(kernels, filters, paddings, strides)(x)
+                    x = tf.keras.layers.BatchNormalization()(x)
+                    x = tf.keras.layers.LeakyReLU(0.1)(x) 
+            else:
+                kernels, filters, paddings, strides = struct['kernel'], struct['filters'], struct['padding'], struct['strides']
+                x = tf.keras.layers.MaxPooling2D(
+                                pool_size=(strides, strides),
+                                strides=strides, padding="same",
+                                )(x)
+        i+=1
+# x = tf.keras.layers.Flatten()(x)
+x = tf.keras.layers.Dense(4096)(x)
+x = tf.keras.layers.LeakyReLU(0.1)(x)
+# x = tf.keras.layers.Reshape((7,7,30))(x)
+
+x = tf.keras.layers.Dense(30,activation='linear')(x)
+# x = tf.keras.layers.LeakyReLU(0.1)(x)
+# x = tf.keras.layers.Reshape((7,7,30))(x)
+
+model = tf.keras.Model(inp,x)
+
+print(model.summary())
